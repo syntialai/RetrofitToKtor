c9623b3838b3ae90611ede33f2d52b3b8ebdbc9e
Syntia <syntia@gdn-commerce.com>
2022-05-28 17:22:52 +0700

    Bugfix

--- core/src/main/java/com/syntia/moviecatalogue/base/utils/ImageUtils.kt
+++ core/src/main/java/com/syntia/moviecatalogue/base/utils/ImageUtils.kt
@@ -3,7 +3,6 @@ package com.syntia.moviecatalogue.base.utils
 import android.content.Context
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
-import androidx.appcompat.content.res.AppCompatResources
 import com.bumptech.glide.Glide
 
 object ImageUtils {
@@ -13,9 +12,15 @@ object ImageUtils {
   const val PLACEHOLDER_TYPE_POSTER = 0
   const val PLACEHOLDER_TYPE_CAST = 1
 
-  fun loadImage(context: Context, imagePath: String?, imageView: ImageView,
-      @DrawableRes placeholder: Int) {
-    Glide.with(context).load("$IMAGE_API_URL$imagePath").placeholder(placeholder).into(
-        imageView).onLoadFailed(AppCompatResources.getDrawable(context, placeholder))
+  fun loadImage(
+      context: Context,
+      imagePath: String?,
+      imageView: ImageView,
+      @DrawableRes placeholderResId: Int) {
+    Glide.with(context)
+        .load("$IMAGE_API_URL$imagePath")
+        .placeholder(placeholderResId)
+        .error(placeholderResId)
+        .into(imageView)
   }
 }
